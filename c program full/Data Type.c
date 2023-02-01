ea877dcc5a41aa64454194ae647d0613700353a2
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-02-01 22:27:35 +0600

    Create Data Type.c

---
+++ c program full/Data Type.c
@@ -0,0 +1,15 @@
+//write a program that print integer, float, character and double data type.
+#include<stdio.h>
+int main()
+{
+ int num1 = 20;
+ float num2 = 5.54;
+ double num3 = 10.40656;
+ char num4 = 'l';
+
+ printf("integer number is %d\n", num1);
+ printf("floating number is %f\n", num2);
+ printf("double number is %lf\n", num3);
+ printf("character number is %c", num4);
+ getch();
+}
