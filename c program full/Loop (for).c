299fbef92788cd36d48a5b950d77aa5b1dc18ead
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-02-01 22:29:47 +0600

    Create Loop (for).c

---
+++ c program full/Loop (for).c
@@ -0,0 +1,29 @@
+/*
+In for loop there are three parts
+1. initialization
+2. condition
+3. counter update
+*/
+/*
+in this program,
+i=1 (this part is initialization
+i<=5 (this part is condition)
+i++ (this part is counter update)
+*/
+// for double statement we have to use second bracket in every statement.
+
+#include<stdio.h>
+int main()
+{
+    int i;
+    int z;
+    for (i=1; i<=5; i++)
+    printf("abdul hamim leon\n");
+
+    for (z=1; z<=10; z++) //double statement
+    {
+        printf("Hamim\n");
+        printf("%d\n",z);
+    }
+    getch ();
+}
