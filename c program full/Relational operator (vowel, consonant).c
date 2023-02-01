0fd5e947ceff29ced3b4c441197bc32ea6b44680
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-02-01 22:33:20 +0600

    Create Relational operator (vowel, consonant).c

---
+++ c program full/Relational operator (vowel, consonant).c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+char ch;
+    printf("Enter any character: ");
+    scanf("%c", &ch);
+
+    if (ch== 'a' )
+        printf("Vowel");
+
+    else if (ch== 'e' )
+        printf("Vowel");
+
+    else if (ch== 'i' )
+        printf("Vowel");
+
+    else  if (ch== 'o' )
+        printf("Vowel");
+
+    else if (ch== 'u' )
+        printf("Vowel");
+
+        else
+            printf("Consonant");
+
+        getch ();
+
+}
