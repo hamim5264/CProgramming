df274fd8e8187ec1615bba778d74933d1af5134d
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-02-01 22:36:29 +0600

    Create Structure-5(Array of structure).c

---
+++ c program full/Structure-5(Array of structure).c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+struct person
+{
+    char name[50];
+    int age;
+    float salary;
+};
+int main ()
+{
+struct person person[3];
+int i;
+
+for (i=0; i<3; i++)
+{
+    printf("Enter information for person %d\n", i+1);
+    printf("Enter name : ");
+    scanf("%s", &person[i].name);
+    printf("Enter age : ");
+    scanf("%d", &person[i].age);
+    printf("Enter salary: ");
+    scanf("%f", &person[i].salary);
+}
+
+for (i=0; i<3; i++)
+{
+    printf("\n\nInformation for person %d\n", i+1);
+    printf("Name : %s\n", person[i].name);
+    printf("Age : %d\n", person[i].age);
+    printf("Salary : %f", person[i].salary);
+}
+
+getch ();
+}
