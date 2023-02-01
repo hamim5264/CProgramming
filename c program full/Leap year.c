2a908975f1f8f8b921956b34282d01000b1d7da8
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-02-01 22:29:29 +0600

    Create Leap year.c

---
+++ c program full/Leap year.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int year;
+    printf("Enter any year: ");
+    scanf("%d", &year);
+
+    if (year%400==0)
+        printf("The year is leap year");
+
+    else if (year%4==0 && year%100!=0)
+        printf("The year is leap year");
+
+    else
+        printf("Not leap year");
+    getch ();
+
+}
