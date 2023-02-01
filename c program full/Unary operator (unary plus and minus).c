ef1d4c0372ec8b9217ab54779e5eacf9d0e3417a
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-02-01 22:37:29 +0600

    Create Unary operator (unary plus and minus).c

---
+++ c program full/Unary operator (unary plus and minus).c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int x, result  ;
+    printf("Enter any number for unary plus: ");
+    scanf("%d", &x);
+    result = -x;
+    printf("Result = %d ",result);
+    return 0;
+}
