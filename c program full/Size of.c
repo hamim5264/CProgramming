ffd788e89fe793b33d4fbd512c9dc14717ab1faa
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-02-01 22:34:45 +0600

    Create Size of.c

---
+++ c program full/Size of.c
@@ -0,0 +1,14 @@
+//size of all variables.
+#include<stdio.h>
+int main()
+{
+    int G ;
+    float T ;
+    double H ;
+    char F ;
+    printf("size of integer: %d bytes\n", sizeof (G));
+    printf("size of floating: %d bytes\n", sizeof (T));
+    printf("size of double: %d bytes\n", sizeof (H));
+    printf("size of character: %d bytes\n", sizeof (F));
+    getch ();
+}
