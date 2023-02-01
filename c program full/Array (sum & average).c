f0cb2cfa570d1a93fdfdb0847c514822613e4c23
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-02-01 22:25:21 +0600

    Create Array (sum & average).c

---
+++ c program full/Array (sum & average).c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main ()
+{
+    int a[ 5 ], sum=0, i;
+    a[0]= 10;
+    a[1]=20;
+    a[2]=30;
+    a[3]=40;
+    a[4]=50;
+    for (i=0; i<=5; i++)
+    {
+        sum = sum + a[i];
+    }
+    printf("sum = %d\n", sum);
+    printf("average = %f", (float) sum/5 );
+    getch ();
+}
