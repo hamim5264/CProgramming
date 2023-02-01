bc962671cc835c49079f28825208e6374c0636d7
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-02-01 22:33:30 +0600

    Create Relational operator(if,if).c

---
+++ c program full/Relational operator(if,if).c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int x;
+    printf("enter an integer number: ");
+    scanf("%d", &x);
+    if (x%2 ==0)
+        printf("your number is even\n");
+    if (x%2 !=0);
+    printf("your number is odd");
+
+    getch ();
+}
