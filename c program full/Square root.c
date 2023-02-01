99fb541987b9ef369486214f962fd3832273cf96
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-02-01 22:34:52 +0600

    Create Square root.c

---
+++ c program full/Square root.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    double x, result = sqrt(x);
+    printf("Enter any value: \n");
+    scanf("%lf", &x);
+    result = sqrt (x);
+    printf("Result is:\n%lf", result);
+    return 0 ;
+}
