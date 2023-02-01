0db3358bd93ff11fb3364e930073126480a6552f
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-02-01 22:26:47 +0600

    Create ASCII value to Character.c

---
+++ c program full/ASCII value to Character.c
@@ -0,0 +1,13 @@
+/*
+In ASCII value capital letter starts with 65 and small letter starts with 97
+*/
+#include<stdio.h>
+int main()
+{
+    int h;
+    printf("Enter any ASCII number: ");
+    scanf("%d", & h);
+    printf("The ASCII Character is: %c\n",h);
+
+    return 0;
+}
