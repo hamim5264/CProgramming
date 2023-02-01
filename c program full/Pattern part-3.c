a3b2d80f270c54213c87f4b13b511c7e684bbc92
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-02-01 22:31:37 +0600

    Create Pattern part-3.c

---
+++ c program full/Pattern part-3.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,col,row;
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    for (row=1; row<=n; row++)
+    {
+        for (col=1; col<=row; col++)
+        {
+            printf("%c ", col+64); //for character output and 65 is capital ascii value
+        }
+        printf("\n");
+    }
+    getch ();
+
+}
+
