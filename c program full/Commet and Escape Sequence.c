1117127d2438e848b30c8678b8142d45655469fc
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-02-01 22:27:18 +0600

    Create Commet and Escape Sequence.c

---
+++ c program full/Commet and Escape Sequence.c
@@ -0,0 +1,11 @@
+/*
+write a program that prints your name and age and your contact number
+*/
+#include<stdio.h>
+int main ()
+{
+printf("Name: Leon \n"); //my name
+printf("Age: 21 \n"); //my age
+printf("Contact: 01724879284"); //my phone number
+return 0;
+}
